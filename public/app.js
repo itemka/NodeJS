a2cbaf570ea83d73b6c9d006ffc40686ca1d60af
Artsem Paulenka <artsem.paulenka@yellow.systems>
2020-09-21 00:38:55 +0300

    Сonnecting client scripts

---
+++ public/app.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.price').forEach(node => {
+  node.textContent = new Intl.NumberFormat(
+      'en-EN', {
+        currency: 'usd',
+        style: 'currency'
+      }
+    )
+    .format(node.textContent);
+})
